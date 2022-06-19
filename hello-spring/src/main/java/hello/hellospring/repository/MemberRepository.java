1d0f9551e6ff2fb7370a2c9b80a55b4d9d0e17c7
HyeonUk kim <kimdo20@naver.com>
2022-06-19 12:10:28 +0900

    Update test case 작성

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 
 public interface MemberRepository {
     Member save(Member member);
-    Optional<Member> findByID(Long id); //optional은 자바 8부터 나온 기능 NPE 방지를 위한 코드임
-    Optional<Member> findByName(Long id);
+    Optional<Member> findById(Long id); //optional은 자바 8부터 나온 기능 NPE 방지를 위한 코드임
+    Optional<Member> findByName(String name);
     List<Member> findAll();
 
 }
